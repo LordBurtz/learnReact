85ffbc805fa9c7aa0a88020e7f3498ee41f123d7
LordBurtz <fridolin.karger@gmail.com>
2022-01-16 18:03:21 +0100

    tut learn

---
+++ index.js
@@ -0,0 +1,14 @@
+import ReactDOM from "react"
+
+const navbar = (
+    <nav>
+        <h1>[brand]</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navbar, document.getElementById("root"))
